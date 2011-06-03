2db14b6dd13b8b22ae121232a40d30ba0f01cf00
Mikhail Gusarov <dottedmag@dottedmag.net>
2011-06-03 10:19:58 +0200

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ kw = {
     'author': "Mikhail Gusarov",
     'author_email': "dottedmag@dottedmag.net",
     'url': "http://github.com/dottedmag/path.py",
-    'license': "Public domain",
+    'license': "MIT License",
     'py_modules': ['path', 'test_path'],
     'cmdclass': dict(build_py=build_py),
 }
@@ -27,7 +27,7 @@ if hasattr(distutils.core, 'setup_keywords'):
     if 'classifiers' in distutils.core.setup_keywords:
         kw['classifiers'] = [
             'Development Status :: 5 - Production/Stable',
-            'License :: Freely Distributable',
+            'License :: OSI Approved :: MIT License',
             'Intended Audience :: Developers',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
