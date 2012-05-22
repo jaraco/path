f7e4c468272c47924f0f8286d376e89f5d3ccbdb
Pedro Romano <pedro.romano@digitaluna.net>
2012-05-22 22:56:58 +0100

    Fix mismatched version in 'test_path'.

--- test_path.py
+++ test_path.py
@@ -22,7 +22,7 @@ import codecs, os, random, shutil, tempfile, time
 from path import path, __version__ as path_version
 
 # This should match the version of path.py being tested.
-__version__ = '2.2.1.990'
+__version__ = '2.2.2.990'
 
 
 def p(**choices):
