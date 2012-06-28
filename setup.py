bc682fc12127e924b81ca34754b3e6bccd5351f9
Jason R. Coombs <jaraco@jaraco.com>
2012-06-28 12:08:46 -0400

    Updated Trove classifiers to specify the Python versions supported

--- setup.py
+++ setup.py
@@ -33,6 +33,12 @@ if hasattr(distutils.core, 'setup_keywords'):
             'Intended Audience :: Developers',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2.3',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
             'Topic :: Software Development :: Libraries :: Python Modules'
           ]
 
