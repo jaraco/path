1dacea2fb5fd52bff0853a0132dbcbdf2effd8fb
Jason R. Coombs <jaraco@jaraco.com>
2012-10-01 11:25:01 -0400

    Remove compatibility for Python 2.2.2 and earlier. Python 2.3 is required, as indicated by the classifiers.

--- setup.py
+++ setup.py
@@ -9,38 +9,33 @@ except ImportError:
 
 # Setup script for path
 
-kw = {
-    'name': "path.py",
-    'version': "2.4.1",
-    'description': "A module wrapper for os.path",
-    'author': "Jason Orendorff",
-    'author_email': "jason.orendorff@gmail.com",
-    'maintainer': "Jason R. Coombs",
-    'maintainer_email': "jaraco@jaraco.com",
-    'url': "http://github.com/jaraco/path.py",
-    'license': "MIT License",
-    'py_modules': ['path', 'test_path'],
-    'cmdclass': dict(build_py=build_py),
-}
-
-
-# If we're running Python 2.3, add extra information
-if hasattr(distutils.core, 'setup_keywords'):
-    if 'classifiers' in distutils.core.setup_keywords:
-        kw['classifiers'] = [
-            'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: MIT License',
-            'Intended Audience :: Developers',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.3',
-            'Programming Language :: Python :: 2.4',
-            'Programming Language :: Python :: 2.5',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Topic :: Software Development :: Libraries :: Python Modules'
-          ]
+kw = dict(
+    name = "path.py",
+    version = "2.4.1",
+    description = "A module wrapper for os.path",
+    author = "Jason Orendorff",
+    author_email = "jason.orendorff@gmail.com",
+    maintainer = "Jason R. Coombs",
+    maintainer_email = "jaraco@jaraco.com",
+    url = "http://github.com/jaraco/path.py",
+    license = "MIT License",
+    py_modules = ['path', 'test_path'],
+    cmdclass = dict(build_py=build_py),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
 
 
 distutils.core.setup(**kw)
