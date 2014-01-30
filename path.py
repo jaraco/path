2cd10f93153752d931eeb61d19f7262d060adda6
Jason R. Coombs <jaraco@jaraco.com>
2014-01-30 10:55:15 -0500

    Incorporate 'inplace' from Martijn Pieters. Fixes #53.

--- path.py
+++ path.py
@@ -60,6 +60,7 @@ import tempfile
 import functools
 import operator
 import re
+import contextlib
 
 try:
     import win32security
@@ -1357,6 +1358,73 @@ class path(unicode):
             """ .. seealso:: :func:`os.startfile` """
             os.startfile(self)
             return self
+
+    # in-place re-writing, courtesy of Martijn Pieters
+    # http://www.zopatista.com/python/2013/11/26/inplace-file-rewriting/
+    @contextlib.contextmanager
+    def inplace(filename, mode='r', buffering=-1, encoding=None, errors=None,
+                newline=None, backup_extension=None):
+        """Allow for a file to be replaced with new content.
+
+        yields a tuple of (readable, writable) file objects, where writable
+        replaces readable.
+
+        If an exception occurs, the old file is restored, removing the
+        written data.
+
+        mode should *not* use 'w', 'a' or '+'; only read-only-modes are supported.
+
+        """
+        import io
+
+        # move existing file to backup, create new file with same permissions
+        # borrowed extensively from the fileinput module
+        if set(mode).intersection('wa+'):
+            raise ValueError('Only read-only file modes can be used')
+
+        backupfilename = filename + (backup_extension or os.extsep + 'bak')
+        try:
+            os.unlink(backupfilename)
+        except os.error:
+            pass
+        os.rename(filename, backupfilename)
+        readable = io.open(backupfilename, mode, buffering=buffering,
+                           encoding=encoding, errors=errors, newline=newline)
+        try:
+            perm = os.fstat(readable.fileno()).st_mode
+        except OSError:
+            writable = open(filename, 'w' + mode.replace('r', ''),
+                            buffering=buffering, encoding=encoding, errors=errors,
+                            newline=newline)
+        else:
+            os_mode = os.O_CREAT | os.O_WRONLY | os.O_TRUNC
+            if hasattr(os, 'O_BINARY'):
+                os_mode |= os.O_BINARY
+            fd = os.open(filename, os_mode, perm)
+            writable = io.open(fd, "w" + mode.replace('r', ''), buffering=buffering,
+                               encoding=encoding, errors=errors, newline=newline)
+            try:
+                if hasattr(os, 'chmod'):
+                    os.chmod(filename, perm)
+            except OSError:
+                pass
+        try:
+            yield readable, writable
+        except Exception:
+            # move backup back
+            try:
+                os.unlink(filename)
+            except os.error:
+                pass
+            os.rename(backupfilename, filename)
+            raise
+        finally:
+            readable.close()
+            writable.close()
+            try:
+                os.unlink(backupfilename)
+            except os.error:
+                pass
 
 
 class tempdir(path):
