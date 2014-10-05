b73a7e908fa0a4da3e1b5bba335d4e234fe015f5
Jason R. Coombs <jaraco@jaraco.com>
2014-10-05 12:24:37 -0400

    Bumped to 7.0 in preparation for next release.

--- path.py
+++ path.py
@@ -105,7 +105,7 @@ def io_error_compat():
 
 ##############################################################################
 
-__version__ = '6.3'
+__version__ = '7.0'
 __all__ = ['Path', 'path', 'CaseInsensitivePattern']
 
 
