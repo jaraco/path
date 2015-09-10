befb74de5c73e65d00aa8e264d5ae1cbe6132b78
Jason R. Coombs <jaraco@jaraco.com>
2015-09-10 09:58:46 +0200

    Also restore path to __all__ to prevent failure on packages that using import *. Ref #102

--- path.py
+++ path.py
@@ -1707,9 +1707,14 @@ class CaseInsensitivePattern(text_type):
     def normcase(self):
         return __import__('ntpath').normcase
 
-
+########################
+# Backward-compatibility
 class path(Path):
     def __new__(cls, *args, **kwargs):
         msg = "path is deprecated. Use Path instead."
         warnings.warn(msg, DeprecationWarning)
         return Path.__new__(cls, *args, **kwargs)
+
+
+__all__ += ['path']
+########################
